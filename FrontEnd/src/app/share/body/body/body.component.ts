1ff0eae17dc03ecee43831078fb36d7942d46e8a
Nicolas Ariza <naariza@uniboyaca.edu.co>
2021-02-27 12:32:05 -0500

    27/02/2021

---
+++ FrontEnd/src/app/share/body/body/body.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styles: [
+  ]
+})
+export class BodyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
