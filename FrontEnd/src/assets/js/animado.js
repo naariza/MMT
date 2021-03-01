09d00408deed7737f499ef6e44d03fbc26a7e15b
Nicolas Ariza <naariza@uniboyaca.edu.co>
2021-03-01 17:07:48 -0500

    01/03/2021

--- FrontEnd/src/assets/js/animado.js
+++ FrontEnd/src/assets/js/animado.js
@@ -1,8 +1,15 @@
 
 window.addEventListener('scroll',function(){
     let line=document.querySelectorAll(".card-line-time")[0];
-    let altura=window.innerHeight;
+    let altura=window.innerHeight/1.3;
     var distancia =line.getBoundingClientRect().top;
 
-    console.log(distancia)
+    if(distancia<=altura)
+{
+line.classList.add('aparece');
+}  
+else{
+line.classList.remove('aparece');
+}  
+// console.log(distancia)
 });
